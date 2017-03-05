2503e7be87c26f4fa2aa30239492239c14c846a0
Lonka Valtteri J <vlonka@da3-cs-dk110-13.helsinki.fi>
2017-03-06 01:05:23 +0200

    samaan kurssiin ei monta ilmoa

--- app/controllers/ilmoittautuminen_controller.php
+++ app/controllers/ilmoittautuminen_controller.php
@@ -2,17 +2,21 @@
 
 class Ilmoittautuminen_Controller extends BaseController {
 
-
     public static function ilmo($id) {
         self::check_logged_in();
         $oppilas = $_SESSION['opiskelija'];
-        
-        $query = DB::connection()->prepare('INSERT INTO Ilmoittautuminen (opiskelijaid, kurssi_id) VALUES (:opiskelijaid, :kurssi_id) RETURNING id');
-        $query->execute(array('opiskelijaid' => $oppilas, 'kurssi_id' => $id));
 
+        $query1 = DB::connection()->prepare('SELECT * FROM Ilmoittautuminen WHERE opiskelijaid = :opiskelijaid AND kurssi_id = :kurssi_id LIMIT 1');
+        $query1->execute(array('opiskelijaid' => $oppilas, 'kurssi_id' => $id));
+        $haku = $query1->fetch();
+
+        if ($haku == NULL) {
+            $query = DB::connection()->prepare('INSERT INTO Ilmoittautuminen (opiskelijaid, kurssi_id) VALUES (:opiskelijaid, :kurssi_id) RETURNING id');
+            $query->execute(array('opiskelijaid' => $oppilas, 'kurssi_id' => $id));
+        }
+        
         Redirect::to('/kurssit');
     }
-
 
     public static function destroy($id) {
         self::check_logged_in();
